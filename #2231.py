37405f0906da66258efd7ffac95b3b26cdd03582
hykoh0103 <100256830+hykoh0103@users.noreply.github.com>
2022-07-11 20:58:32 +0900

    Update and rename #2231 to #2231.py

--- #2231.py
+++ #2231.py
@@ -1,14 +1,14 @@
-def f_power(num):
+def f_power(num): #몇자리의 수인지 구하는 함수(6자리 이하)
     for i in range(6):
         if num / pow(10, i) < 1:
             return i
 
-def f_separate(n):
+def f_separate(n): #입력한 수를 숫자를 쪼개어 리스트로 저장하는 함수
     ans = []
     while n > 0:
         temp = int(n%10)
         ans.insert(0, temp)
-        n = int((n - temp) / 10) #int() 추가해서 name-error 해결
+        n = int((n - temp) / 10) #int() 추가하니까 name-error 해결되었다.
         
     while len(ans) != 6:
         ans.insert(0, 0)   
@@ -17,7 +17,7 @@ def f_separate(n):
 
 num = int(input())
 p = f_power(num)
-target = num - 9 * p
+target = num - 9 * p #각 자리의 수가 9 이하 이므로, 9*p보다 작은수부터 생성자인지 조사하면 된다고 생각했다. 
 
 while target < num:
     temp = f_separate(target)
